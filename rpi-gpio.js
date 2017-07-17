4a80b867e69226e0f7895f2ea88f136aea649a07
Kiran Kota <kiranmkota@gmail.com>
2017-07-17 13:25:09 -0700

    fix bcm channel validation

--- rpi-gpio.js
+++ rpi-gpio.js
@@ -347,34 +347,7 @@ function Gpio() {
 
     function getPinBcm(channel) {
         channel = parseInt(channel, 10);
-        return [
-            3,
-            5,
-            7,
-            8,
-            10,
-            11,
-            12,
-            13,
-            15,
-            16,
-            18,
-            19,
-            21,
-            22,
-            23,
-            24,
-            26,
-            29,
-            31,
-            32,
-            33,
-            35,
-            36,
-            37,
-            38,
-            40
-        ].indexOf(channel) !== -1 ? (channel + '') : null;
+        return (channel >= 0 && channel <= 27) ? (channel + '') : null;
     };
 
     /**
