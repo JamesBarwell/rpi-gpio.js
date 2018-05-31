e8b3a143c7d2e57fa81818d45ff55fd01c5c6843
James Barwell <jb@jamesbarwell.co.uk>
2018-05-31 14:15:53 +0100

    Revert "format code with prettier"
    This reverts commit 7e83f8357d91db327ce60f991066556fb0d63b7c.

--- test/integration/main.js
+++ test/integration/main.js
@@ -4,64 +4,64 @@ var assert = require('assert');
 var sinon = require('sinon');
 
 var message =
-  'Please ensure that your Raspberry Pi is set up with physical pins ' +
-  '7 and 11 connected via a 1kΩ resistor (or similar) to make this test work';
-console.log(message);
-console.log('');
-console.log('|-----------------------------------...');
-console.log('|-  [02][04][06][08][10][12][14][16]...');
-console.log('|-  [01][03][05][07][09][11][13][15]...');
-console.log('|-                |      |          ...');
-console.log('|-                |-<1k>-|          ...');
-console.log('');
+    'Please ensure that your Raspberry Pi is set up with physical pins ' +
+    '7 and 11 connected via a 1kΩ resistor (or similar) to make this test work'
+console.log(message)
+console.log('')
+console.log('|-----------------------------------...')
+console.log('|-  [02][04][06][08][10][12][14][16]...')
+console.log('|-  [01][03][05][07][09][11][13][15]...')
+console.log('|-                |      |          ...')
+console.log('|-                |-<1k>-|          ...')
+console.log('')
 
-var writePin = 7;
-var readPin = 11;
+
+var writePin = 7
+var readPin = 11
 
 describe('rpi-gpio integration', function() {
-  var readValue;
-  var onChange = sinon.spy();
 
-  before(function(done) {
-    gpio.on('change', onChange);
+    var readValue;
+    var onChange = sinon.spy()
 
-    async.waterfall(
-      [
-        function(next) {
-          gpio.setup(writePin, gpio.DIR_OUT, next);
-        },
-        function(next) {
-          gpio.setup(readPin, gpio.DIR_IN, gpio.EDGE_BOTH, next);
-        },
-        function(next) {
-          gpio.write(writePin, 1, next);
-        },
-        function(next) {
-          setTimeout(next, 100);
-        },
-        function(next) {
-          gpio.read(readPin, function(err, value) {
-            readValue = value;
-            next();
-          });
-        },
-      ],
-      function(err) {
-        done(err);
-      },
-    );
-  });
+    before(function(done) {
+        gpio.on('change', onChange);
 
-  after(function(done) {
-    gpio.destroy(done);
-  });
+        async.waterfall([
+            function(next) {
+                gpio.setup(writePin, gpio.DIR_OUT, next)
+            },
+            function(next) {
+                gpio.setup(readPin, gpio.DIR_IN, gpio.EDGE_BOTH, next)
+            },
+            function(next) {
+                gpio.write(writePin, 1, next);
+            },
+            function(next) {
+                setTimeout(next, 100)
+            },
+            function(next) {
+                gpio.read(readPin, function(err, value) {
+                    readValue = value;
+                    next();
+                });
+            }
+        ], function(err) {
+            done(err)
+        });
+    });
 
-  it('should trigger the change listener', function() {
-    sinon.assert.calledOnce(onChange);
-    sinon.assert.calledWith(onChange, 11, true);
-  });
+    after(function(done) {
+        gpio.destroy(done)
+    });
 
-  it('should set the read pin on', function() {
-    assert.equal(readValue, true);
-  });
+    it('should trigger the change listener', function() {
+        sinon.assert.calledOnce(onChange)
+        sinon.assert.calledWith(onChange, 11, true)
+    });
+
+    it('should set the read pin on', function() {
+        assert.equal(readValue, true)
+    });
+
 });
